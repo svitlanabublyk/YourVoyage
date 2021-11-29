874a4307c880b6d783bab291f3059a8e828e84d1
Svitlana Bublyk <lana.bublyk@gmail.com>
2021-11-29 18:51:40 +0100

    create iOS project

---
+++ YourVoyage/YourVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourVoyageApp.swift
+//  YourVoyage
+//
+//  Created by Svitlana Bublyk on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct YourVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
